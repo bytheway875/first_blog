cd88fe0cae8cf93f8042c46c9c44a0d9bd17719d
MakerSquare Student <student@themakersquare.com>
2013-07-02 15:14:41 -0500

    Added delete and update views and actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,46 @@
 class PostsController < ApplicationController
   def index
-    @name = "Shehzan"
+    @name = "Shannon"
     @posts = Post.all
   end
+
+  # def create
+  #   Post.create(params)
+  # end
 
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post
+  end
+
+  def delete
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 FirstBlog::Application.routes.draw do
   get '/posts' => 'posts#index'
   get '/post/:id' => 'posts#show', as: 'post'
+  get '/posts/new' => 'posts#new'
+  post '/posts' => 'posts#create'
+  get 'post/:id/edit' => 'posts#edit'
+  put 'post/:id' => 'posts#update'
+  delete 'post/:id' => 'posts#delete'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
